cd274ce838b3626c4050956b4f912dae0d7b4533
Devarata <devarata.oza@gmail.com>
2020-03-10 13:25:32 +0530

    Folder algorithms add

---
+++ Algorithms/basic.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin>>t;
+	cout<<v.begin()<<"\n";
+	cout<<v.end()<<"\n";
+
+	return 0;
+}
